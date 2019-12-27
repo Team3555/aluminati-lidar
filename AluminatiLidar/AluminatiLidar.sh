ced3d32393050477bc5e4a3b4d2b2b12f30d857f
ch515 <precisioncomputerguy515@gmail.com>
2019-12-27 13:04:28 -0500

    Converted to unix

--- AluminatiLidar/AluminatiLidar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-LD_LIBRARY_PATH=/home/pi/AluminatiLidar/jni chrt --rr 99 /home/pi/AluminatiLidar/jre/bin/java -Xcomp -Xmx128M -XX:+UseG1GC -XX:MaxGCPauseMillis=10 -XX:InitiatingHeapOccupancyPercent=50 -jar /home/pi/AluminatiLidar/AluminatiLidar.jar
